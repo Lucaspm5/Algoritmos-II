a829d5cad9bd0d4ad90744765f0519e13e7745c8
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-03-14 19:02:32 -0300

    Update p1.c

--- Trabalho/p1.c
+++ Trabalho/p1.c
@@ -1,133 +1,195 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-
-#define MAX_WORDS 10000
-#define MAX_WORD_LEN 50
-#define MAX_SENTENCES 1000
-#define MAX_SENT_LEN 200
-
-int is_stopword(char* word, char** stopwords, int num_stopwords) {
-    int i;
-    for (i = 0; i < num_stopwords; i++) {
-        if (strcmp(word, stopwords[i]) == 0) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-int count_words(char* text) {
-    int num_words = 0;
-    char* word = strtok(text, " ");
-    while (word != NULL) {
-        num_words++;
-        word = strtok(NULL, " ");
-    }
-    return num_words;
-}
-
-void remove_stopwords(char* text, char** stopwords, int num_stopwords) {
-    char* word = strtok(text, " ");
-    while (word != NULL) {
-        if (is_stopword(word, stopwords, num_stopwords)) {
-            strcpy(word, "");
-        }
-        word = strtok(NULL, " ");
-    }
-}
-
-int count_sentences(char* text) {
-    int num_sentences = 0;
-    char* sentence = strtok(text, ".!?");
-
-    while (sentence != NULL) {
-        num_sentences++;
-        sentence = strtok(NULL, ".!?");
-    }
-    return num_sentences;
-}
-
-void summarize_text(char* input_path, char* output_path, int summary_size, char* stopwords_path) {
-    char** stopwords = (char**) malloc(MAX_WORDS * sizeof(char*));
-    char* text = (char*) malloc(MAX_WORDS * MAX_WORD_LEN * sizeof(char));
-    char* summary = (char*) malloc(MAX_SENTENCES * MAX_SENT_LEN * sizeof(char));
-    char* line = (char*) malloc(MAX_SENT_LEN * sizeof(char));
-    FILE* input_file = fopen(input_path, "r");
-    FILE* output_file = fopen(output_path, "w");
-    FILE* stopwords_file = fopen(stopwords_path, "r");
-    int num_stopwords = 0;
-    int num_words = 0;
-    int num_sentences = 0;
-    int summary_length = 0;
-    int i, j;
 
 
-    while (fgets(line, MAX_SENT_LEN, stopwords_file)) {
-        stopwords[num_stopwords] = (char*) malloc(MAX_WORD_LEN * sizeof(char));
-        strcpy(stopwords[num_stopwords], line);
-        num_stopwords++;
-    }
-  
-    while (fgets(line, MAX_SENT_LEN, input_file)) {
-        strcat(text, line);
-    }
+#define TAM 50
+#define TAM2 1000
 
-    remove_stopwords(text, stopwords, num_stopwords);
+char stopwords[][TAM] = {"a", "as", "o", "os", "um", "uns", "uma", "umas", "para", "de", "da", "do", "das", "dos", "se", "entao", "ele", "ela", "eles", "elas", "ou", "por", "que", "com", "tambem"};
+char stopwords_ingles[][TAM] = {"the", "a", "an", "at", "by", "from", "how", "of", "on", "that", "to", "for", "in", "out", "I", "he", "she", "it", "they", "them", "him", "her", "do", "will", "or", "with", "also"};
 
-    
-    num_words = count_words(text);
-    num_sentences = count_sentences(text);
-
-    char** sentences = (char**) malloc(num_sentences * sizeof(char*));
-    sentence = strtok(text, ".!?");
-    i = 0;
-    while (sentence != NULL) {
-        sentences[i] = (char*) malloc(MAX_SENT_LEN * sizeof(char));
-        strcpy(sentences[i], sentence);
-        sentence = strtok(NULL, ".!?");
-        i++;
-    }
-
-    for (i = 0; i < num_sentences; i++) {
-        int sentence_length = count_words(sentences[i]);
-        if (summary_length + sentence_length <= summary_size) {
-            strcat(summary, sentences[i]);
-            strcat(summary, ". ");
-            summary_length += sentence_length;
-        }
-   
-    fprintf(output_file, "%s", summary);
-
-    for (i = 0; i < num_stopwords; i++) {
-        free(stopwords[i]);
-    }
-        
-    free(stopwords);
-    free(text);
-    free(summary);
-        
-    for (i = 0; i < num_sentences; i++) {
-        free(sentences[i]);
-    }
-    free(sentences);
-    fclose(input_file);
-    fclose(output_file);
-    fclose(stopwords_file);
-}
-
-int main(int argc, char* argv[]) 
+int num_stopwords = 26;
+int num_stopwords_ingles = 27;
+/////////////////////////////////////////////////////////////////////////////////////////////
+void imprimir_palavras_arquivo(FILE *arquivo, char palavras[TAM2][TAM], int num_palavras);
+void contar_ocorrencias(FILE *arquivo,char palavras[TAM2][TAM], int num_palavras);
+void ordenar(int *vetor, int num_palavras, char palavras[TAM2][TAM]);
+void ve_quantidade(char palavras[TAM2][TAM], int num_palavras, int *vetor);
+/////////////////////////////////////////////////////////////////////////////////////////////
+void main(int argc, char **argv)
 {
-    if (argc < 5) 
+
+    FILE *arquivo = fopen("teste.txt", "r");
+    char linha[TAM2];
+    char palavras[TAM2][TAM];
+    int num_palavras = 0, tam_palavra, i, j;
+
+    if (arquivo == NULL)
     {
-        printf("Usage: summarize_text input_path output_path summary_size stopwords_path\n");
+        printf("Erro ao abrir o arquivo\n");
         return 1;
     }
-    char* input_path = argv[1];
-    char* output_path = argv[2];
-    int summary_size = atoi(argv[3]);
-    char* stopwords_path = argv[4];
-    summarize_text(input_path, output_path, summary_size, stopwords_path);
+
+    fgets(linha, TAM2, arquivo);
+
+
+    char *palavra = strtok(linha, " \n\r\t");
+    while(palavra != NULL && num_palavras < TAM2)
+    {
+        tam_palavra = strlen(palavra);
+        for(i = 0, j = 0; i < tam_palavra; i++)
+        {
+            if(!ispunct(palavra[i]))
+            {
+                palavra[j++] = tolower(palavra[i]);
+            }
+        }
+        palavra[j] = '\0';
+        strcpy(palavras[num_palavras], palavra);
+        palavra = strtok(NULL, " \n\r\t");
+        num_palavras++;
+    }
+
+    fclose(arquivo);
+
+    FILE *arquivo2 = fopen("teste2.txt","a");
+    if (arquivo2 == NULL)
+    {
+        printf("Erro ao abrir o arquivo\n");
+        return 1;
+    }
+
+    imprimir_palavras_arquivo(arquivo2, palavras, num_palavras);
+
+    fclose(arquivo2);
+
+    FILE *arq = fopen("teste3.txt","a");
+    if (arq == NULL)
+    {
+        printf("Erro ao abrir o arquivo\n");
+        return 1;
+    }
+    contar_ocorrencias(arquivo,palavras, num_palavras);
+
+    fclose(arq);
+
+
+
     return 0;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+void imprimir_palavras_arquivo(FILE *arquivo, char palavras[TAM2][TAM], int num_palavras)
+{
+    int i, j;
+    for (i = 0; i < num_palavras; i++)
+    {
+        for(j = 0; j < num_palavras; j++)
+        {
+            if (strcmp(palavras[i], stopwords[j]) == 0)
+            {
+                fprintf(arquivo, "%s, ", palavras[i]);
+            }
+        }
+    }
+    for(i = 0; i < num_palavras; i++)
+    {
+        for(j = 0; j < num_palavras; j++)
+        {
+            if(strcmp(palavras[i], stopwords_ingles[j]) == 0)
+            {
+                fprintf(arquivo, "%s, ",palavras[i]);
+            }
+        }
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+void contar_ocorrencias(FILE *arquivo,char palavras[TAM2][TAM], int num_palavras)
+{
+    int ocorrencia[TAM2] = {0};
+    int i, j, k, qnt;
+    int ocorrencia_total[TAM2] = {0};
+
+    // contar as ocorrencias de cada palavra
+    for(i = 0; i < num_palavras; i++)
+    {
+        // Verifica se a palavra atual é uma stopwords
+        int eh_stopwords = 0;
+        for (k = 0; k < num_stopwords; k++)
+        {
+            if (strcmp(palavras[i], stopwords[k]) == 0)
+            {
+                eh_stopwords = 1;
+                break;
+            }
+        }
+
+        if (!eh_stopwords)
+        {
+            int palavra_ja_contada = 0;
+            for(j = 0; j < i; j++)
+            {
+                if(strcmp(palavras[i], palavras[j]) == 0)
+                {
+                    palavra_ja_contada = 1;
+                    break;
+                }
+            }
+
+            if(!palavra_ja_contada)
+            {
+                for(j = 0; j < num_palavras; j++)
+                {
+                    if(strcmp(palavras[i], palavras[j]) == 0)
+                    {
+                        ocorrencia[i]++;
+                    }
+                }
+            }
+        }
+    }
+
+    // classificar o vetor de ocorrências em ordem decrescente com o booble soft
+    ordenar(ocorrencia, num_palavras, palavras);
+
+    for(i = 0; i < num_palavras; i++)
+    {
+        if(ocorrencia[i] > 0)
+        {
+            printf("palavra: %s, ocorrencia: %d\n",palavras[i],ocorrencia[i]);
+            fprintf(arquivo,"palavra: %s, ocorrencia: %d\n",palavras[i],ocorrencia[i]);
+        }
+    }
+    printf("quantas palavras deseja ver: ");
+    scanf("%d",&qnt);
+    ve_quantidade(palavras, qnt, ocorrencia);
+}
+void ordenar(int *vetor, int num_palavras, char palavras[TAM2][TAM])
+{
+    int i, j;
+    for(i = 0; i < num_palavras - 1; i++)
+    {
+        for(j = i + 1; j < num_palavras; j++)
+        {
+            if(vetor[i] < vetor[j])
+            {
+                int temp = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = temp;
+
+                char temp_palavra[TAM];
+                strcpy(temp_palavra, palavras[i]);
+                strcpy(palavras[i], palavras[j]);
+                strcpy(palavras[j], temp_palavra);
+            }
+        }
+    }
+}
+
+void ve_quantidade(char palavras[TAM2][TAM], int num_palavras, int *vetor)
+{
+    int i;
+    for(i = 0; i < num_palavras; i++)
+    {
+         printf("palavra: %s, ocorrencia: %d\n",palavras[i],vetor[i]);
+    }
 }
