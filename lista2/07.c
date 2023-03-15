202986624252c236d24ac5e0ec81beee6ad077a7
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-03-15 14:21:58 -0300

    Update and rename A-1015.c to 07.c

---
+++ lista2/07.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int valores[10] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int elementos[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int i;
+
+    int *p_valores = valores;
+    int *p_elementos = elementos;
+
+    printf("Elemento\tValor\n");
+    for (i = 0; i < 10; i++) {
+        printf("%d\t%d\n", *(p_elementos + i), *(p_valores + i));
+    }
+
+    return 0;
+}
+
