7bf9a5d948e02beda65928e1ac9c7548b3baee68
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-03-15 15:21:24 -0300

    Update 5.c

--- lista3/5.c
+++ lista3/5.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int x=0;
-int y = 0;
-int tentativas = 0;
-
-int repeticao(){
+void repeticao(int *x, int *y, int *tentativas){
     int lower = 1, upper = 1000;
-    x = rand() % (upper - lower + 1) + lower;
-    while (y != x)
+    *x = rand() % (upper - lower + 1) + lower;
+    while (*y != *x)
     {
-        scanf("%d", &y);
-        if (y < x)
+        scanf("%d", y);
+        if (*y < *x)
         {
             printf("Muito baixo. Tente Novamente.\n");
-            tentativas++;
-        }else if (y > x)
+            (*tentativas)++;
+        }
+        else if (*y > *x)
         {
             printf("Muito alto. Tente Novamente.\n");
-            tentativas++;
+            (*tentativas)++;
         } 
     }
-    printf("Excelente! Voce adivinhou o numero!\n");
-    printf("Numero de tentativas %d", tentativas);
+    printf("Excelente! Você adivinhou o número!\n");
+    printf("Número de tentativas: %d\n", *tentativas);
 }
-
 
 int main()
 {
     srand(time(NULL));
-    printf("-Advinhe o numero-\n");
-    repeticao();
+    printf("-Adivinhe o número-\n");
+    int x = 0;
+    int y = 0;
+    int tentativas = 0;
+    repeticao(&x, &y, &tentativas);
+    return 0;
 }
