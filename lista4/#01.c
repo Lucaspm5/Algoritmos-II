48c9dfeb30c024c3f2f31ed59722c5af6c4ddba8
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-03-14 21:52:25 -0300

    Update #01.c

--- lista4/#01.c
+++ lista4/#01.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define TAM 5
 
@@ -6,53 +8,72 @@ typedef struct
 {
     char nome[15];
     float peso, altura, Ira;
-}aluno;
+} aluno;
+
+void calcular_medias(aluno *c, int tam, float *Mpeso, float *Maltura, float *Mira)
+{
+    *Mpeso = 0;
+    *Maltura = 0;
+    *Mira = 0;
+    for (int i = 0; i < tam; i++)
+    {
+        *Mpeso += c[i].peso;
+        *Maltura += c[i].altura;
+        *Mira += c[i].Ira;
+    }
+    *Mpeso /= tam;
+    *Maltura /= tam;
+    *Mira /= tam;
+}
+
+int achar_maximo(aluno *c, int tam, char *campo)
+{
+    int indice_max = 0;
+    for (int i = 1; i < tam; i++)
+    {
+        if (*campo == 'peso' && c[i].peso > c[indice_max].peso)
+        {
+            indice_max = i;
+        }
+        else if (*campo == 'altura' && c[i].altura > c[indice_max].altura)
+        {
+            indice_max = i;
+        }
+    }
+    return indice_max;
+}
 
 int main(void)
 {
     aluno c[TAM];
-    int sup1, sup2, sup3;
-    float Mpeso, Maltura, Mira, maxPeso = 0, maxAlt = 0, maxIra = 0;
+    float Mpeso, Maltura, Mira;
+
     printf("\t-CADASTRO DE ALUNOS-\n\n");
-    for(int aux=0; aux<TAM; aux++)
+
+    for (int i = 0; i < TAM; i++)
     {
-        printf("Aluno - %d\n",aux + 1);
+        printf("Aluno - %d\n", i + 1);
         printf("Informe o nome: ");
-        printf("\n");
-        fgets(c[aux].nome,15,stdin);
-        printf("Informe a altura, peso e ira: ");
-        printf("\n");
-        scanf("%f %f %f", &c[aux].altura, &c[aux].peso, &c[aux].Ira);
-
-        Maltura+=c[aux].altura;
-        Mpeso+=c[aux].peso;
-        Mira+=c[aux].Ira;
-
-        if (maxPeso < c[aux].peso)
+        fgets(c[i].nome, 15, stdin);
+        c[i].nome[strcspn(c[i].nome, "\n")] = '\0'; // remover o caractere '\n' da string
+        printf("Informe a altura, peso e IRA: ");
+        if (scanf("%f %f %f", &c[i].altura, &c[i].peso, &c[i].Ira) != 3)
         {
-            maxPeso = c[aux].peso;
-            sup1 = aux;
+            printf("Erro ao ler os valores do aluno %d. Certifique-se de que eles sejam números.\n", i + 1);
+            exit(1);
         }
-
-        if (maxAlt < c[aux].altura)
-        {
-            maxAlt = c[aux].altura;
-            sup2 = aux;
-        }
-
-        if (maxIra < c[aux].Ira)
-        {
-            maxIra = c[aux].Ira;
-            sup3 = aux;
-        }
-
         fflush(stdin);
     }
 
-    printf("\nMedia de Peso: %.2f\n", Mpeso/=TAM);
-    printf("Media de Altura: %.2f\n", Maltura/=TAM);
-    printf("Media de IRA: %.2f\n", Mira/=TAM);
-    printf("Maior Peso: %s %.2f\n", c[sup1].nome, maxPeso);
-    printf("Maior Altura: %s %.2f\n",c[sup2].nome , maxAlt);
+    calcular_medias(c, TAM, &Mpeso, &Maltura, &Mira);
+    int indice_max_peso = achar_maximo(c, TAM, "peso");
+    int indice_max_altura = achar_maximo(c, TAM, "altura");
+
+    printf("\nMedia de Peso: %.2f\n", Mpeso);
+    printf("Media de Altura: %.2f\n", Maltura);
+    printf("Media de IRA: %.2f\n", Mira);
+    printf("Maior Peso: %s %.2f\n", c[indice_max_peso].nome, c[indice_max_peso].peso);
+    printf("Maior Altura: %s %.2f\n", c[indice_max_altura].nome, c[indice_max_altura].altura);
+
     return 0;
 }
