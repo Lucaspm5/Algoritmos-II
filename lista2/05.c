97e3216ac3993ae735e71a8f918aa0d717328659
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-03-15 14:47:39 -0300

    Update 05.c

--- lista2/05.c
+++ lista2/05.c
@@ -1,83 +1,91 @@
 #include <stdio.h>
-#define tam 5
+#include <string.h>
 
-int main()
-{
+#define TAM 5
 
-    char nome[tam][20];
-    float mediaAltura, somaAltura=0, menorAltura, maiorAltura;
-    float mediaIra, somaIra=0, maiorIra, menorIra, somaPeso=0, mediaPeso, maiorPeso, menorPeso, dados[tam][3];
-    int i, j, mp, mi, ma;
-    printf("Informe os seguintes dados: nome, altura, peso, ira\n");
-    for(i=0; i<tam; i++){
-        scanf("%s",nome[i]);
-        for(j=0; j<3; j++){
-            scanf("%f",&dados[i][j]);
+typedef struct{
+    char nome[20];
+    float altura;
+    float peso;
+    float ira;
+} Aluno;
+
+void lerAluno(Aluno *aluno){
+    printf("Nome: ");
+    scanf("%s", aluno->nome);
+    printf("Altura: ");
+    scanf("%f", &aluno->altura);
+    printf("Peso: ");
+    scanf("%f", &aluno->peso);
+    printf("IRA: ");
+    scanf("%f", &aluno->ira);
+}
+
+void imprimirAluno(Aluno aluno){
+    printf("%-20s  %.2f  %.2f  %.2f\n", aluno.nome, aluno.altura, aluno.peso, aluno.ira);
+}
+
+int encontrarMaiorAltura(Aluno alunos[]){
+    int i, indice = 0;
+    float maiorAltura = alunos[0].altura;
+
+    for (i = 1; i < TAM; i++){
+        if (alunos[i].altura > maiorAltura){
+            maiorAltura = alunos[i].altura;
+            indice = i;
         }
     }
 
-    maiorAltura=dados[0][0];
-    menorAltura=dados[0][0];
-    menorIra=dados[0][1];
-    menorIra=dados[0][1];
-    maiorPeso=dados[0][2];
-    menorPeso=dados[0][2];
+    return indice;
+}
 
-    for(i=0; i<tam; i++){
-        for(j=0; j<3; j++){
-            if(j==0){
-                somaAltura+=dados[i][0];
-            }
-            if(j==1){
-                somaPeso+=dados[i][1];
-            }
-            if(j==2){
-                somaIra+=dados[i][2];
-            }
-            if(dados[i][0]>maiorAltura){
-                maiorAltura=dados[i][0];
-                ma=i;
-            }
-            if(dados[i][0]<menorAltura){
-                menorAltura=dados[i][0];
-            }
-            if(dados[i][1]>maiorIra){
-                maiorIra=dados[i][1];
-                mi=i;
-            }
-            if(dados[i][1]<menorIra){
-                menorIra=dados[i][1];
-            }
-            if(dados[i][2]>maiorPeso){
-                maiorPeso=dados[i][2];
-                mp=i;
-            }
-            if(dados[i][2]<menorPeso){
-                menorPeso=dados[i][2];
-            }
+int encontrarMenorAltura(Aluno alunos[]){
+    int i, indice = 0;
+    float menorAltura = alunos[0].altura;
+
+    for (i = 1; i < TAM; i++){
+        if (alunos[i].altura < menorAltura){
+            menorAltura = alunos[i].altura;
+            indice = i;
         }
     }
 
-    mediaAltura=somaAltura/tam;
-    mediaIra=somaIra/tam;
-    mediaPeso=somaPeso/tam;
+    return indice;
+}
 
-    printf("Maior altura: %.2f\nMenor altura: %.2f\nMedia das alturas: %.2f\n",maiorAltura,menorAltura,mediaAltura);
-    printf("Maior Ira: %.2f\nMenor Ira: %.2f\nMedia dos Iras: %.2f\n",maiorIra,menorIra,mediaIra);
-    printf("Maior peso: %.2f\nMenor peso: %.2f\nMedia dos pesos: %.2f\n",maiorPeso,menorPeso,mediaPeso);
+int encontrarMaiorIRA(Aluno alunos[]){
+    int i, indice = 0;
+    float maiorIRA = alunos[0].ira;
 
-    for(int l=0; l<tam; l++){
-        for(int j=0; j<3; j++){
-            if(l==ma)
-                printf("Dados do aluno com maior Altura:\nNome: %s\nAltura: %.2f\nPeso: %.2f\nIra: %.2f\n",nome[l],dados[l][j],dados[l][j],dados[l][j]);
-
-            if(l==mi)
-                printf("Dados do aluno com maior Ira:\nNome: %s\nAltura: %.2f\nPeso: %.2f\nIra: %.2f\n",nome[l],dados[l][j],dados[l][j],dados[l][j]);
-
-            if(l==mp)
-                printf("Dados do aluno com maior Peso:\nNome: %s\nAltura: %.2f\nPeso: %.2f\nIra: %.2f\n",nome[l],dados[l][j],dados[l][j],dados[l][j]);
-        }
+    for (i = 1; i < TAM; i++){
+        if (alunos[i].ira > maiorIRA){
+            maiorIRA = alunos[i].ira;
+            indice = i;
+            }
     }
+    return indice;
+}
+
+int main() {
+    Aluno turma[TAM];
+    int i, indiceMaiorAltura, indiceMenorAltura, indiceMaiorIRA;
+
+    for (i = 0; i < TAM; i++) {
+        printf("Aluno %d:\n", i+1);
+        lerAluno(&turma[i]);
+    }
+
+    indiceMaiorAltura = encontrarMaiorAltura(turma);
+    printf("\nAluno com maior altura:\n");
+    imprimirAluno(turma[indiceMaiorAltura]);
+
+    indiceMenorAltura = encontrarMenorAltura(turma);
+    printf("\nAluno com menor altura:\n");
+    imprimirAluno(turma[indiceMenorAltura]);
+
+    indiceMaiorIRA = encontrarMaiorIRA(turma);
+    printf("\nAluno com maior IRA:\n");
+    imprimirAluno(turma[indiceMaiorIRA]);
 
     return 0;
 }
