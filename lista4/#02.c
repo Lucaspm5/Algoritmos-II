49f98dfff84e7c6f9836ecb54250d765cd131f57
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-03-14 21:54:28 -0300

    Update #02.c

--- lista4/#02.c
+++ lista4/#02.c
@@ -1,35 +1,27 @@
-#include<stdio.h>
-#include<math.h>
+include <stdio.h>
+#include <math.h>
 
-typedef struct
-{
-    float x1,x2,y1,y2;
-}distancia;
+typedef struct {
+    float x1, x2, y1, y2;
+} distancia;
 
-int main()
-{
-    float subx1,suby2;
-    float mutx1,muty2;
-    float soma,res;
-    float total;
-
-
+int main() {
+    float distancia_x, distancia_y, soma, res;
     distancia dois_pontos;
-    fflush(stdin);
-    scanf("%f %f",&dois_pontos.x1,&dois_pontos.x2);
-    fflush(stdin);
-    scanf("%f %f",&dois_pontos.y1,&dois_pontos.y2);
 
-    subx1=dois_pontos.x1 - dois_pontos.y1;
-    suby2=dois_pontos.x2 - dois_pontos.y2;
-    
-    mutx1=subx1*subx1;
-    muty2=suby2*suby2;
-    
-    soma=mutx1+muty2;
-    res=sqrt(soma);
-    total=res;
-    
-    printf("%.4f",total);
+    printf("Digite as coordenadas do primeiro ponto (x1 y1): ");
+    scanf("%f %f", &dois_pontos.x1, &dois_pontos.y1);
+
+    printf("Digite as coordenadas do segundo ponto (x2 y2): ");
+    scanf("%f %f", &dois_pontos.x2, &dois_pontos.y2);
+
+    distancia_x = dois_pontos.x2 - dois_pontos.x1;
+    distancia_y = dois_pontos.y2 - dois_pontos.y1;
+
+    soma = pow(distancia_x, 2) + pow(distancia_y, 2);
+    res = sqrt(soma);
+
+    printf("A distancia entre os pontos eh: %.4f\n", res);
+
     return 0;
 }
