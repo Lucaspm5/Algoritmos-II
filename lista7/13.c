d82567a124a19c0156d594b7ac1028faca4416f2
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-03-17 10:29:31 -0300

    Update 13.c

--- lista7/13.c
+++ lista7/13.c
@@ -1,41 +1,157 @@
 #include <stdio.h>
+#include <time.h>
 
-int main(void)
+typedef struct{
+	int chave;
+	char nomes[30];
+	char endereco[50];
+	char telefone[14];
+}Alunos;
+////////////////////////////////
+void gerarDados(Alunos *v, int n, int ordemChave);
+void bolha(Alunos *v, int n);
+void selecao(Alunos *v, int n);
+void insercao(Alunos *v, int n);
+////////////////////////////////
+int main()
 {
-  int n;
-
-  scanf("%d", &n);
-
-  int auxiliar_1 = 0;
-  int auxiliar_3 = 0;
-
-  while (n > 0)
-  {
-    int copy = n % 10;//Exemplo: se a entrada fosse 200 ou seja o resto 0, o programa se encerraria
-
-    if (copy == 1) {//Caso o resto fosse 1 entraria no if
-      auxiliar_1 = 1;// e o auxiliar_1 seria atribuido o valor 1
-    }
-
-    else if (copy == 3) {//O resto agora vale 3
-      auxiliar_3 = 1;//1 é atribuido ao auxiliar_3
-    }
-
-    if (auxiliar_1 && auxiliar_3)
-    {
-      printf("es de Mala Suerte\n");
-      return 0;//return 0 dentro de laços pode servir como um break
-    }
-
-    n /= 10;/*Um meio para impedir um futuro laço infinito, verificando casa decimal por casa decimal
-             No exemplo do 13 entrando com N
-             Copy = 3
-             auxiliar_3 = 1
-             N agora valeria 1,3
-             auxiliar_1 = 1
-             printf("es de Mala Suerte");
-             */
-  }
-  printf("NO es de Mala Suerte\n");
-  return 0;
+	int n,c, ordem, alg;
+	scanf("%d",&n);
+	Alunos aluno[n];
+	while(1)
+	{
+		scanf("%d %d",&ordem,&alg);
+		if(ordem == 3 || alg == 3)
+			break;
+		if(alg == 0)
+		{
+			printf("bolha\n");
+			gerarDados(aluno, n, ordem);
+			for(c = 0; c < n; c++)
+			{
+				printf("%d ",aluno[c].chave);
+			}
+			printf("\n");
+			bolha(aluno, n);
+			for(c = 0; c < n; c++)
+			{
+				printf("%d ",aluno[c].chave);
+			}
+			printf("\n");
+		}
+		if(alg == 1)
+		{
+			printf("selecao\n");
+			gerarDados(aluno, n, ordem);
+			for(c = 0; c < n; c++)
+			{
+				printf("%d ",aluno[c].chave);
+			}
+			printf("\n");
+			selecao(aluno, n);
+			for(c = 0; c < n; c++)
+			{
+				printf("%d ",aluno[c].chave);
+			}
+			printf("\n");
+		}
+		if(alg == 2)
+		{
+			printf("insercao\n");
+			gerarDados(aluno, n, ordem);
+			for(c = 0; c < n; c++)
+			{
+				printf("%d ",aluno[c].chave);
+			}
+			printf("\n");
+			insercao(aluno, n);
+			for(c = 0; c < n; c++)
+			{
+				printf("%d ",aluno[c].chave);
+			}
+			printf("\n");
+		}
+	}
+} 
+////////////////////////////////
+void gerarDados(Alunos *v, int n, int ordemChave)
+{
+	int i, j, c;
+	if(ordemChave == 0)
+	{
+		for(c = 0; c < n; c++)
+		{
+            v[c].chave = c;
+        }
+	}
+	if(ordemChave == 1)
+	{
+		srand(time(NULL));
+		for(c = 0; c < n; c++)
+		{
+			 v[c].chave = rand() % 1000;
+		}
+	}
+	if(ordemChave == 2)
+	{
+		for(c = n-1, j = 0; c >= 0; c--, j++)
+		{
+			 v[j].chave = c;
+		}
+	}
+}
+////////////////////////////////
+void bolha(Alunos *v, int n)
+{
+	int i, j;
+	Alunos aux;
+	for(i = 0; i < n-1; i++)
+	{
+		for(j = 1; j < n-i; j++)
+		{
+			if(v[j].chave < v[j-1].chave)
+			{
+				aux = v[j];
+				v[j] = v[j-1];
+				v[j-1] = aux;
+			}
+		}
+	}
+}
+////////////////////////////////
+void selecao(Alunos *v, int n)
+{
+	int i, j, min;
+	Alunos aux;
+	for(i = 0; i < n; i++)
+	{
+		min = i;
+		for(j = i + 1; j < n; j++)
+		{
+			if(v[j].chave < v[min].chave)
+			{
+				min = j;
+			}
+		}
+		aux = v[i];
+		v[i] = v[min];
+		v[min] = aux;
+	}
+}
+////////////////////////////////
+void insercao(Alunos *v, int n)
+{
+	int i, j;
+	Alunos aux;
+	for(i = 1; i < n; i++)
+	{
+		aux = v[i];
+		j = i - 1;
+		while((j >= 0) && (aux.chave < v[j].chave))
+		{
+			v[j+1]= v[j];
+			j--;
+		}
+		v[j+1] = aux;
+	}
 }
