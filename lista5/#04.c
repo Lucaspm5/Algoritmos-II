6b2a63890c57d2a3871f36de155274947f1e774d
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-04-03 14:54:15 -0300

    Update #04.c

--- lista5/#04.c
+++ lista5/#04.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
-void somaCom(float rz1, float iz1, float rz2, float iz2, float *rz3, float *iz3 );
-float rz1, iz1, rz2, iz2, rz3 = 0, iz3 = 0;
+void somaComplexo(float rz1, float iz1, float rz2, float iz2, float *rz3, float *iz3);
 
-int main(void)
-{
-   scanf("%f %f %f %f", &rz1, &iz1, &rz2, &iz2);
-   somaCom(rz1, iz1, rz2, iz2, &rz3, &iz3);
-   printf("%.1f + %.1fi\n", rz3, iz3);
+int main() {
+    float rz1, iz1, rz2, iz2, rz3, iz3;
+    scanf("%f", &rz1);
+    while (rz1 != 0) {
+        scanf("%f %f %f", &iz1, &rz2, &iz2);
+        somaComplexo(rz1, iz1, rz2, iz2, &rz3, &iz3);
+        if (iz3 < 0) {
+            printf("%.1f - %.1fi\n", rz3, -iz3);
+        } else {
+            printf("%.1f + %.1fi\n", rz3, iz3);
+        }
+        scanf("%f", &rz1);
+    }
+    return 0;
 }
 
-void somaCom(float rz1, float iz1, float rz2, float iz2, float *rz3, float *iz3 )
-{
-   *rz3 = rz1 + rz2;
-   *iz3 = iz1 + iz2;
+void somaComplexo(float rz1, float iz1, float rz2, float iz2, float *rz3, float *iz3) {
+    *rz3 = rz1 + rz2;
+    *iz3 = iz1 + iz2;
 }
