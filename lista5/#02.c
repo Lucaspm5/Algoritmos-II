191f780994db6b6ac851f1670f1b9ab4c14bddf9
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-04-03 14:51:23 -0300

    Update #02.c

--- lista5/#02.c
+++ lista5/#02.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <math.h>
 
-int bhaskara(float a, float b, float c,float *r1, float *r2) 
-{
-    float delta = b*b - 4*a*c;
-    int raizes_reais = (delta >= 0 ? 1 : 0);
-
-    if(raizes_reais)
-    {
-        (*r1) = (-b + sqrt(delta))/(2*a);
-        (*r2) = (-b - sqrt(delta))/(2*a);
+void raizes(float a, float b, float c, float *x1, float *x2) {
+    float delta = b * b - 4 * a * c;
+    
+    if(delta >= 0) {
+        *x1 = (-b + sqrt(delta)) / (2 * a);
+        *x2 = (-b - sqrt(delta)) / (2 * a);
+    } else {
+        *x1 = *x2 = NAN;
     }
-
-    return raizes_reais;
 }
 
-int main(float a, float b, float c)
-{
-    float r1, r2;
+int main() {
+    float a, b, c, x1, x2;
+    
     scanf("%f", &a);
-    scanf("%f", &b);
-    scanf("%f", &c);
-    int raizes_reais;
-    raizes_reais = bhaskara(a, b, c, &r1, &r2);
-    if(raizes_reais)
-        printf("%.1f %.1f\n", r1, r2);
-    else
-        printf("complexo.\n");
+    
+    while(a != 0) {
+        scanf("%f %f", &b, &c);
+        raizes(a, b, c, &x1, &x2);
+        
+        if(isnan(x1) || isnan(x2)) {
+            printf("complexo\n");
+        } else {
+            printf("%.1f %.1f\n", x1, x2);
+        }
+        
+        scanf("%f", &a);
+    }
+    
+    return 0;
 }
