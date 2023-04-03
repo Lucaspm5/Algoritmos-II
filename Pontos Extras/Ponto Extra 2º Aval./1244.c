e5558d8403dd6989875118caf8d736c2b35c18bc
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-04-03 13:31:33 -0300

    Update and rename 1039.c to 1244.c

---
+++ Pontos Extras/Ponto Extra 2º Aval./1244.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void insertion_sort(char** v, int n)
+{
+    for (int i = 1; i < n; i++) {
+        char* key = v[i];
+        int j = i - 1;
+        while (j >= 0 && strlen(v[j]) < strlen(key))
+        {
+            v[j + 1] = v[j];
+            j--;
+        }
+        v[j + 1] = key;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int qtd;
+    char s[1001];
+    char* v[1001];
+    char* aux;
+    scanf("%d", &qtd);
+
+    getchar();
+
+    while (qtd--)
+    {
+        int count = 0;
+        fgets(s, 1001, stdin);
+        aux = strtok(s, " \n");
+        while (aux != NULL)
+        {
+            v[count++] = strdup(aux);
+            aux = strtok(NULL, " \n");
+        }
+
+        insertion_sort(v, count);
+
+        for (int i = 0; i < count; i++)
+        {
+            if (i != 0)
+                printf(" ");
+            printf("%s", v[i]);
+            free(v[i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
