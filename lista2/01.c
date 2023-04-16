0a462a4c4c49b664caa926e356227009bbea4f52
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-04-16 16:51:53 -0300

    Update 01.c

--- lista2/01.c
+++ lista2/01.c
@@ -1,52 +1,51 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 #define tam 40
-#define aux 100
+#define size 100
 
-int remove_duplicates(int input[], int n, int output[]) 
+int verifica_se_eh_duplicada(int entrada[], int n, int flag[])
 {
     int cont = 0;
-    int seen[tam] = {0};
+    int auxiliary[tam] = {0};
 
-    for (int i = 0; i < n; i++) {
-        if (!seen[input[i]]) {
-            seen[input[i]] = 1;
-            output[cont++] = input[i];
+    for(int i = 0; i < n; i++)//percorre os 40 elementos
+    {
+        if(auxiliary[entrada[i]] == 0)//se o elemento ainda não foi encontrado ele entra no loop
+        {
+            flag[cont++] = entrada[i];//o elemento não repetido sera adicionado ao novo vetor que a cada condição valida acrescentara um valor valido ao vetor
+            auxiliary[entrada[i]] = 1;//permite que o elemento adicionado ao flag n venha a se repetir
         }
     }
 
     return cont;
 }
 
-int main() {
+int main(int argc, char const **argv)
+{
+    srand(time(NULL));
+
     int entrada[tam] = {0};
-    int flag[aux] = {0};
+    int flag[size] = {0};
     int n = tam;
 
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &entrada[i]);
+    for(int i = 0; i < n; i++)
+    {
+        entrada[i] = rand() % 10;//Preenche os 40 elementos no vetor
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (entrada[i] > entrada[j]) {
-                int temp = entrada[i];
-                entrada[i] = entrada[j];
-                entrada[j] = temp;
-            }
-        }
-    }
+    int auxiliar = verifica_se_eh_duplicada(entrada, n, flag);//chama a função
 
-    int auxiliar = remove_duplicates(entrada, n, flag);
-
-    for (int i = 0; i < auxiliar; i++) {
+    for(int i = 0; i < auxiliar; i++)//auxiliar esta recebendo o retorno de cont(que é a qntd de elementos sem repetir)
+    {
         int cont = 0;
-        for (int j = 0; j < n; j++) {
-            if (flag[i] == entrada[j]) {
+        for(int j = 0; j < n; j++)//j ta percorrendo os 40 elementos da entrada
+        {
+            if(flag[i] == entrada[j])//se o flag que tem 9 valores por exemplo, forem igual a um dos 40 sera contado
                 cont++;
-            }
         }
-        printf("%d\t %d\n", flag[i], cont);
+        printf("%d\t %d\n", flag[i], cont);// ira printar o elemento e quantas vezes ele se repetiu
     }
 
     return 0;
