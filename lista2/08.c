117f6c3cff42916e1bf300c1ebe86222c903f98d
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-04-16 18:44:59 -0300

    Update 08.c

--- lista2/08.c
+++ lista2/08.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-const int SIZE = 10;
+#define SIZE 10
 
 int valor[SIZE] = {0};
-int elemento[SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+int elemento[SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
 
 int main() {
     for (int i = 0; i < SIZE; i++){
