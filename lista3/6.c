6d09b9eb2ba3bdb0339db231b995f6ff9b431434
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-05-03 23:30:01 -0300

    Update 6.c

--- lista3/6.c
+++ lista3/6.c
@@ -4,24 +4,25 @@
 
 #define TAM 5
 
-int main()
-{
-    char *artigo[TAM] = {"o", "um", "algum", "todo", "qualquer"};
-    char *substantivo[TAM] = {"menino", "homem", "cachorro", "carro", "gato"};
-    char *verbo[TAM] = {"passou", "pulou", "correu", "saltou", "andou"};
-    char *preposicao[TAM] = {"sobre", "sob", "ante", "ate", "com"};
+char *artigo[TAM] = {"o", "um", "algum", "todo", "qualquer"};
+char *substantivo[TAM] = {"menino", "homem", "cachorro", "carro", "gato"};
+char *verbo[TAM] = {"passou", "pulou", "correu", "saltou", "andou"};
+char *preposicao[TAM] = {"sobre", "sob", "ante", "ate", "com"};
 
+void generateSentence(char *sentence) {
+    int a = rand() % TAM;
+    int s = rand() % TAM;
+    int v = rand() % TAM;
+    int p = rand() % TAM;
+    sprintf(sentence, "%s %s %s %s %s %s", artigo[a], substantivo[s], verbo[v], preposicao[p], artigo[a], substantivo[s]);
+}
+
+int main() {
     srand(time(NULL));
-
-    for (int i = 0; i < 20; i++)
-    {
-        int a = rand() % TAM;
-        int s = rand() % TAM;
-        int v = rand() % TAM;
-        int p = rand() % TAM;
-
-        printf("%d - %s %s %s %s %s %s\n", i+1, artigo[a], substantivo[s], verbo[v], preposicao[p], artigo[a], substantivo[s]);
+    char sentence[100];
+    for (int i = 0; i < 20; i++) {
+        generateSentence(sentence);
+        printf("%d - %s\n", i+1, sentence);
     }
-
     return 0;
 }
